6f1a41800e78f726f4dfd3ed148b5c8182755de6
Vince Turner <vt5491@gmail.com>
2018-12-08 01:15:32 -0800

    linux changes made while processing gs.

--- servers/extractor/insert_text.js
+++ servers/extractor/insert_text.js
@@ -1,21 +1,23 @@
-var mysql = require('mysql')
-const fs = require('fs');
+var mysql = require("mysql");
+const fs = require("fs");
 // const args = require('minimist')(process.argv.slice(2))
-const args = require('yargs').argv;
+const args = require("yargs").argv;
 
-var db = 'super_query'
+var db = "super_query";
 // var corpusTbl = `${db}.text_corpus_dev`
-var corpusTbl = `${db}.text_corpus`
+var corpusTbl = `${db}.text_corpus`;
 // var bookTitle = "The God Equation"
-var bookTitle = args.title
-var series = args.series
-var volume = args.volume
-var limit = args.limit
+var bookTitle = args.title;
+var series = args.series;
+var volume = args.volume;
+var limit = args.limit;
 // var help = args.help
 
 if (args.vthelp) {
   console.log(`Example Call:`);
-  console.log(`node insert_text.js --title="the_god_game" --series="gs" --volume=1 --limit=250`);
+  console.log(
+    `node insert_text.js --title="the_god_game" --series="gs" --volume=1 --limit=250`
+  );
   console.log(`--limit: optional`);
 }
 
@@ -32,54 +34,54 @@ if (!volume) {
   process.exit(1);
 }
 
-let bookName = `${series}${volume}-${bookTitle}.txt`
-let shortTitle = `${series}-${volume}`
+let bookName = `${series}${volume}-${bookTitle}.txt`;
+let shortTitle = `${series}-${volume}`;
 
-var con= mysql.createConnection({
-  host     : 'localhost',
-  user     : process.env.SUPER_QUERY_DB_USER,
-  password : process.env.SUPER_QUERY_DB_PASS,
-  database : 'super_query'
+var con = mysql.createConnection({
+  host: "localhost",
+  user: process.env.SUPER_QUERY_DB_USER,
+  password: process.env.SUPER_QUERY_DB_PASS,
+  database: "super_query"
 });
 
 // fs.readFile("../../dev_artifacts/gs_books/gs6-the_god_equation.txt", "utf8", function(err, data){
-let fn = `../../dev_artifacts/text_books/${bookName}`
+let fn = `../../dev_artifacts/text_books/${bookName}`;
 console.log(`now reading file ${fn}`);
-fs.readFile(fn, "utf8", function(err, data){
-    if(err) throw err;
+fs.readFile(fn, "utf8", function(err, data) {
+  if (err) throw err;
 
-    // var resultArray = //do operation on data that generates say resultArray;
-    con.connect()
+  // var resultArray = //do operation on data that generates say resultArray;
+  con.connect();
 
-    // res.send(resultArray);
-    // clear out any prior records
-    var delSql = `DELETE FROM ${corpusTbl} WHERE short_title="${shortTitle}" AND volume="${volume}"`
-    con.query(delSql, function (err, result) {
-      if (err) throw err;
-      console.log(`records for title=${shortTitle}, volume=${volume} deleted`);
+  // res.send(resultArray);
+  // clear out any prior records
+  var delSql = `DELETE FROM ${corpusTbl} WHERE short_title="${shortTitle}" AND volume="${volume}"`;
+  con.query(delSql, function(err, result) {
+    if (err) throw err;
+    console.log(`records for title=${shortTitle}, volume=${volume} deleted`);
 
-      // let lines = data.split("\n")
-      let lines = data.split("\r\n")
-      console.log(`lines.length=${lines.length}`)
+    // let lines = data.split("\n")
+    let lines = data.split("\r\n");
+    console.log(`lines.length=${lines.length}`);
 
-      let lineStop = args.limit || lines.length;
+    let lineStop = args.limit || lines.length;
 
-      for( let i=0; i< lineStop; i++) {
-        let text = lines[i]
-        text.replace(/[\n\r]/g, '')
-        // console.log(`text=${text}, length=${text.length}`);
-        var insertSql =
-        `INSERT INTO ${corpusTbl} (text, line_num, series, short_title, volume)
+    for (let i = 0; i < lineStop; i++) {
+      let text = lines[i];
+      text = text.replace(/[\n\r]/g, "");
+      text = text.replace(/\"/g, '\\"');
+      // console.log(`text=${text}, length=${text.length}`);
+      var insertSql = `INSERT INTO ${corpusTbl} (text, line_num, series, short_title, volume)
         VALUES ("${text}", ${i}, "${series}", "${shortTitle}", "${volume}")`;
-        con.query(insertSql, function (err, result) {
-          if (err) {
-            console.log(`err: i=${i}, text=${text}`);
-            throw err;
-          }
-          // console.log("1 record inserted");
-        });
-      }
-      console.log(`inserted ${lineStop} records into ${corpusTbl}`);
-      con.end()
-    });
+      con.query(insertSql, function(err, result) {
+        if (err) {
+          console.log(`err: i=${i}, text=${text}`);
+          throw err;
+        }
+        // console.log("1 record inserted");
+      });
+    }
+    console.log(`inserted ${lineStop} records into ${corpusTbl}`);
+    con.end();
+  });
 });
